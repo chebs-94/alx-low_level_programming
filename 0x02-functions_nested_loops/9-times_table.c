1f1ca4a177fcaf2e258e3cd7feaca4f9cbc30952
chebs-94 <mercychebet29@gmail.com>
2022-07-05 15:25:48 -0700

    timetable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,44 +1,30 @@
 #include "main.h"
 
-
-
 /**
  *times_table -> time table
  */
 
 void times_table(void)
-
 {
-
 	int x, y, k;
-
-
 
 	for (x = 0; x < 10; x++)
 	{
-
 		for (y = 0; y < 10; y++)
 
 		{
-
 			k = x * y;
 			if (y == 0)
-
 				_putchar(k + '0');
 
 			if (y != 0 && k < 10)
-
 			{
-
 				_putchar(',');
-
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
 			} else if (k >= 10)
-
 			{
-
 				_putchar(',');
 
 				_putchar(' ');
@@ -46,13 +32,8 @@ void times_table(void)
 				_putchar((k / 10) + '0');
 
 				_putchar((k % 10) + '0');
-
 			}
-
 		}
-
 		_putchar('\n');
-
 	}
-
 }
