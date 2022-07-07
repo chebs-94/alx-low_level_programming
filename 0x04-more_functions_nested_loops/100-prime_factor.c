2c94d208b486fb547beab306c8d58bf025327784
chebs-94 <mercychebet29@gmail.com>
2022-07-07 11:23:10 -0700

    primefactor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,13 +8,22 @@
 int main(void)
 
 {
+
 	long n, i;
+
 	n = 612852475143;
+
 	for (i = 2; i < n; i++)
+
 	{
+
 		while (n % i == 0)
+
 			n = n / i;
+
 	}
+
 	printf("%lu\n", n);
+
 	return (0);
 }
