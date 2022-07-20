bf88ac7552fa51103085a9ebfce54498f412c4a6
chebs-94 <mercychebet29@gmail.com>
2022-07-20 12:27:18 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -19,7 +19,8 @@ int power_operation(int n, int c)
 	}
 	return (0 + power_operation(n, c + 1));
 }
-**
+
+/**
  * _sqrt_recursion - returns the natural square root of a number.
  * @n: input number.
  * Return: natural square root.
