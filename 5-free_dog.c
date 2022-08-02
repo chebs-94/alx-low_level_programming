ee6252b26773278aa822748575512d8d1785b963
chebs-94 <mercychebet29@gmail.com>
2022-08-02 03:02:53 -0700

    dog5

--- 5-free_dog.c
+++ 5-free_dog.c
@@ -1,4 +1,40 @@
 #include "dog.h"
+	
+#include <stdlib.h>
+
+
+
+
+
+/**                                                                                      
+
+ * free_dog - Frees dogs.                                                                
+
+ * @d: The dog to be freed.                                                              
+
+ */
+
+
+
+void free_dog(dog_t *d)
+
+
+
+{
+
+        if (d == NULL)
+
+
+
+                return;
+
+        free(d->owner);
+
+        free(d->name);
+
+        free(d);
+
+} #include "dog.h"
 #include <stdlib.h>
 
 
